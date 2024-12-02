6a6c05b127f547cfbe805b05487fdcac59fcc229
ErickGC546 <erick.galindo.c@tecsup.edu.pe>
2024-12-02 09:58:35 -0500

    Initial Commit

---
+++ MiCalculadora/MiCalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiCalculadoraApp.swift
+//  MiCalculadora
+//
+//  Created by Mac05 on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MiCalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
